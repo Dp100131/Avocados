01e840d45d56db7494055783d0e89edf63a821ad
Daniel Pinto <apintod@uninorte.edu.co>
2022-09-06 13:55:36 -0500

    finish

---
+++ spe.js
@@ -0,0 +1,71 @@
+const profesores = ["Félix Arenas Rueda", "Francisco Javier Garcia", "German Dario Camacho Almeyda", "Harving Diaz Consuegra", "Hector Armando Barrera", "Jesus Alberto Bottet Cervantes", "Jesús Ramírez Montañez", "Jimmy Quintero Gomez", "Julian David De Bedout Ordoñez"];
+
+const profesoras = ["Jimena Lizeth Gomez Delgado", "Kathy Margarita Daza Brochero"];
+
+console.log("Profesores: ");
+console.log();
+
+profesores.forEach(profe => {
+
+    console.log(`Estimado ${profe} 
+    Profesor Cátedra
+    Universidad Industrial de Santander
+    
+    Cordial Saludo,
+    
+    En nombre de SPE UIS Student Chapter, nos complace invitarle a una serie de Jornadas Académicas “Empleos del Futuro en la Industria Oil&Gas” enfocadas a la Transformación Digital en la Educación, dictados por la empresa especializada: ‘Ingeniería 4G’; que, en los últimos años, han desarrollado una serie de investigaciones para determinar la problemática en torno al empleo y con base en esto, están desarrollando una metodología educativa que permita a los jóvenes poder acceder a mejores oportunidades laborales a lo largo de su vida profesional.
+    En total, serán tres jornadas, los días 06, 07 y 08 del mes en curso, cada día se plantea varios núcleos de aprendizaje.
+    En el primero, aprenderemos acerca de los Cambios Generacionales de la Humanidad, los grandes Retos de la Cuarta Revolución Industrial y cuáles son las herramientas y Estrategias que debe tener un estudiante universitario para Posicionarse Profesionalmente y generar condiciones de crecimiento laboral y salarial.
+    Durante el segundo día conoceremos el Funcionamiento de Nuestro Cerebro en el Proceso de Aprendizaje y Cómo podemos incorporar Técnicas Avanzadas de Neurociencias para aprovechar al máximo nuestras capacidades cognitivas en el desarrollo profesional.
+    El tercer día entenderemos la Importancia del Desarrollo de Nuestra Marca Personal para visibilizar nuestros diferenciadores ante potenciales reclutadores en la industria Oil & Gas. 
+    
+     
+     
+    
+    
+    
+    
+    Todas las jornadas se realizarán en el Auditorio La Cira ubicado en el Edificio Jorge Bautista Vesga e iniciarán a las 2:00 PM cada día.  La entrada es libre, no obstante, se cobrará una tarifa opcional por certificar el contenido brindado. Será de 5 USD para estudiantes y 10 USD para docentes.
+    
+    
+    Agradecemos grandemente su atención, estaremos atentos a su respuesta y a cualquier duda que se genere.
+    
+    
+    Atentamente,
+    `);
+
+});
+
+console.log("Profesoras: ");
+console.log();
+
+profesoras.forEach(profe => {
+
+    console.log(`Estimada ${profe} 
+    Profesor Cátedra
+    Universidad Industrial de Santander
+    
+    Cordial Saludo,
+    
+    En nombre de SPE UIS Student Chapter, nos complace invitarle a una serie de Jornadas Académicas “Empleos del Futuro en la Industria Oil&Gas” enfocadas a la Transformación Digital en la Educación, dictados por la empresa especializada: ‘Ingeniería 4G’; que, en los últimos años, han desarrollado una serie de investigaciones para determinar la problemática en torno al empleo y con base en esto, están desarrollando una metodología educativa que permita a los jóvenes poder acceder a mejores oportunidades laborales a lo largo de su vida profesional.
+    En total, serán tres jornadas, los días 06, 07 y 08 del mes en curso, cada día se plantea varios núcleos de aprendizaje.
+    En el primero, aprenderemos acerca de los Cambios Generacionales de la Humanidad, los grandes Retos de la Cuarta Revolución Industrial y cuáles son las herramientas y Estrategias que debe tener un estudiante universitario para Posicionarse Profesionalmente y generar condiciones de crecimiento laboral y salarial.
+    Durante el segundo día conoceremos el Funcionamiento de Nuestro Cerebro en el Proceso de Aprendizaje y Cómo podemos incorporar Técnicas Avanzadas de Neurociencias para aprovechar al máximo nuestras capacidades cognitivas en el desarrollo profesional.
+    El tercer día entenderemos la Importancia del Desarrollo de Nuestra Marca Personal para visibilizar nuestros diferenciadores ante potenciales reclutadores en la industria Oil & Gas. 
+    
+     
+     
+    
+    
+    
+    
+    Todas las jornadas se realizarán en el Auditorio La Cira ubicado en el Edificio Jorge Bautista Vesga e iniciarán a las 2:00 PM cada día.  La entrada es libre, no obstante, se cobrará una tarifa opcional por certificar el contenido brindado. Será de 5 USD para estudiantes y 10 USD para docentes.
+    
+    
+    Agradecemos grandemente su atención, estaremos atentos a su respuesta y a cualquier duda que se genere.
+    
+    
+    Atentamente,
+    `);
+
+});
